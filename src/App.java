c258e20d1700f40ab630fdf90ac99a8cb428c312
Angelica465456 <tramonoto05@gmail.com>
2023-10-13 09:24:29 +0200

    qwerty

--- src/App.java
+++ src/App.java
@@ -8,8 +8,25 @@ import java.time.LocalDateTime;
 public class App {
     public static void main(String[] args) throws Exception{
 		Scanner sca = new Scanner(System.in);
-        LocalDate ora = LocalDate.now();
+        LocalDate oggi = LocalDate.now();
 
-        System.out.println("Adesso: "+ora);
+        System.out.println("Anno scadenza patente: ");
+        int anno = sca.nextInt();
+
+        System.out.println("Mese scadenza paente: ");
+        int mese = sca.nextInt();
+
+        System.out.println("Giorno scadenza patente: ");
+        int giorno = sca.nextInt();
+
+        LocalDate scad = LocalDate.of(anno, mese, giorno);
+
+        scad.compareTo(oggi);
+
+        if(scad.compareTo(oggi) < 0){
+            System.out.println("\nLa patente è scaduta.");
+        }else{
+            System.out.println("\nLa patente è ok.");
+        }
 	}
 }
